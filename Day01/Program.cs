ce23bc1dec17ae9e93681768842127158bd445b7
TadejT <tadej.ternar@analitica.si>
2022-12-01 09:38:08 +0100

    Init

---
+++ Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+
+FileReader reader = new("input.txt");
+
+List<string> lines = reader.ReadStringLines();
+
+List<int> elfs = new();
+int currentCalories = 0;
+for (int i = 0; i < lines.Count; i++)
+{
+  if (int.TryParse(lines[i], out int num))
+  {
+    currentCalories += num;
+  }
+  if (string.IsNullOrWhiteSpace(lines[i]) || i == lines.Count - 1)
+  {
+    elfs.Add(currentCalories);
+    currentCalories = 0;
+  }
+}
+
+Console.WriteLine($"Part 1: {elfs.Max()}");
+Console.WriteLine($"Part 2: {elfs.OrderDescending().Take(3).Sum()}");
---
+++ Shared/FileReader.cs
@@ -0,0 +1,40 @@
+﻿namespace Shared;
+
+public class FileReader
+{
+  private readonly FileInfo _fileInfo;
+
+  public FileReader(string fileName = "input.txt")
+  {
+    _fileInfo = new FileInfo(fileName);
+    if (!_fileInfo.Exists)
+    {
+      throw new Exception("File does not exist");
+    }
+  }
+
+  public List<string> ReadStringLines()
+  {
+    return File.ReadAllLines(_fileInfo.FullName).ToList();
+  }
+
+  public List<int> ReadIntLines()
+  {
+    return ReadStringLines().Select(int.Parse).ToList();
+  }
+
+  public int[,] ReadIntMatrix()
+  {
+    List<string> lines = ReadStringLines();
+    int[,] matrix = new int[lines.Count, lines[0].Length];
+
+    for (int x = 0; x < lines.Count; x++)
+    {
+      for (int y = 0; y < lines[x].Length; y++)
+      {
+        matrix[x, y] = int.Parse(lines[x][y].ToString());
+      }
+    }
+    return matrix;
+  }
+}
