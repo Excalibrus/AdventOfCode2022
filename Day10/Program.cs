68cb401fa1f0220b8a01dc8c128f020ed42b296a
TadejT <tadey.ternar@gmail.com>
2022-12-10 12:38:37 +0100

    P2 done

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -8,6 +8,7 @@ List<string> lines = reader.ReadStringLines();
 
 int x = 1;
 int currentCycle = 0;
+int pixelPosition = 0;
 
 int addxCycles = lines.Count(x => x != "noop") * 2;
 int totalCycles = addxCycles + (addxCycles - lines.Count);
@@ -17,18 +18,32 @@ List<int> checkCycles = Enumerable.Range(0, totalCycles / 40 + 2)
   .Select(n => (n * 40) + 20)
   .ToList();
 
+List<int> checkCrtCycles = Enumerable.Range(0, totalCycles / 40 + 2)
+  .Select(n => n * 40)
+  .ToList();
+
+List<string> crtLines = new List<string>();
+string crtLine = string.Empty;
+
 foreach (string line in lines)
 {
+  List<int> sprite = new List<int> { x - 1, x, x + 1 };
   if (line.Equals("noop"))
   {
+    crtLine += sprite.Contains(pixelPosition) ? "X" : ".";
     currentCycle++;
+    pixelPosition++;
     CheckStrength();
   }
   else if (int.TryParse(line.Split(" ").Last(), out int adding))
   {
+    crtLine += sprite.Contains(pixelPosition) ? "X" : ".";
     currentCycle++;
+    pixelPosition++;
     CheckStrength();
+    crtLine += sprite.Contains(pixelPosition) ? "X" : ".";
     currentCycle++;
+    pixelPosition++;
     CheckStrength();
     x += adding;
   }
@@ -40,11 +55,18 @@ void CheckStrength()
   {
     signalStrengths.Add(currentCycle * x);
   }
+
+  if (checkCrtCycles.Contains(currentCycle))
+  {
+    crtLines.Add(crtLine);
+    crtLine = string.Empty;
+    pixelPosition = 0;
+  }
 }
 
-foreach (int signalStrength in signalStrengths)
+foreach (string crtLinePrint in crtLines)
 {
-  Console.WriteLine(signalStrength);
+  Console.WriteLine(crtLinePrint);
 }
 
 int p1 = signalStrengths.Sum();
