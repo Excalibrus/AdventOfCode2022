b8f9712983b864c03037509e3319d4841ea60278
TadejT <tadey.ternar@gmail.com>
2022-12-12 19:20:40 +0100

    P1 working

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,7 +5,7 @@ using Shared.Objects;
 
 Stopwatch sw = new();
 sw.Start();
-FileReader reader = new("input.txt");
+FileReader reader = new("input_demo.txt");
 
 List<string> lines = reader.ReadStringLines();
 
@@ -18,7 +18,7 @@ for (int row = 0; row < lines.Count; row++)
 {
   for (int col = 0; col < lines[row].Length; col++)
   {
-    countMatrix[row, col] = 0;
+    countMatrix[row, col] = int.MaxValue;
     char letter = lines[row][col];
     if (letter == 'S')
     {
@@ -37,11 +37,11 @@ for (int row = 0; row < lines.Count; row++)
   }
 }
 
-int minPath = int.MaxValue;
+int minPath = 500; // int.max for fuller version
 GoToNextPosition(matrix, new List<MatrixPosition> { startPosition });
 
 sw.Stop();
-Console.WriteLine($"Part one: {minPath + 1}");
+Console.WriteLine($"Part one: {minPath}");
 Console.WriteLine($"Part two: {0}");
 Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
@@ -49,30 +49,35 @@ void GoToNextPosition(char[,] matrix, List<MatrixPosition> path)
 {
   if (path.Count > minPath) return;
   MatrixPosition fromPosition = path.Last();
-  countMatrix[fromPosition.Row, fromPosition.Col]++;
-  Console.WriteLine($"Depth {path.Count}, Min: {minPath}, Elevation: {matrix[fromPosition.Row, fromPosition.Col]}, Size: {countMatrix[fromPosition.Row, fromPosition.Col]}");
-  if (countMatrix[fromPosition.Row, fromPosition.Col] > path.Count) return;
+  if (path.Count < countMatrix[fromPosition.Row, fromPosition.Col])
+  {
+    countMatrix[fromPosition.Row, fromPosition.Col] = path.Count;
+  }
+  else
+  {
+    return;
+  }
+  // Console.WriteLine($"Depth {path.Count}, Min: {minPath}, Elevation: {matrix[fromPosition.Row, fromPosition.Col]}, Size: {countMatrix[fromPosition.Row, fromPosition.Col]}");
+  // if (countMatrix[fromPosition.Row, fromPosition.Col] > path.Count) return;
   List<MatrixPosition> neighbourPositions = matrix.GetNeighbourPositions(fromPosition, MatrixDirection.Cross);
   List<MatrixPosition> possibleNextPositions = neighbourPositions.Where(x =>
     !path.Contains(x) &&
-    (matrix[x.Row, x.Col] == matrix[fromPosition.Row, fromPosition.Col] ||
-     matrix[x.Row, x.Col] == matrix[fromPosition.Row, fromPosition.Col] + 1 ||
-     matrix[x.Row, x.Col] == matrix[fromPosition.Row, fromPosition.Col] - 1
-    )).ToList();
+    matrix[fromPosition.Row, fromPosition.Col] + 1 >= matrix[x.Row, x.Col]
+  ).ToList();
   if (possibleNextPositions.Contains(endPosition))
   {
     if (path.Count <= minPath)
     {
       minPath = path.Count;
-      Console.WriteLine("Found path");
-    //   for (int row = 0; row < matrix.GetLength(0); row++)
-    //   {
-    //     for (int col = 0; col < matrix.GetLength(1); col++)
-    //     {
-    //       Console.Write(path.Contains(new MatrixPosition(row, col)) ? "X" : matrix[row, col]);
-    //     }
-    //     Console.WriteLine();
-    //   }
+      Console.WriteLine($"Found path {minPath}");
+      //   for (int row = 0; row < matrix.GetLength(0); row++)
+      //   {
+      //     for (int col = 0; col < matrix.GetLength(1); col++)
+      //     {
+      //       Console.Write(path.Contains(new MatrixPosition(row, col)) ? "X" : matrix[row, col]);
+      //     }
+      //     Console.WriteLine();
+      //   }
     }
 
     return;
