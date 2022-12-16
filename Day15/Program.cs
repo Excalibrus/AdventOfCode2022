69087796bff027dbecb4dac6b04ec328dc81a7a5
Tadej Ternar <tadej.ternar@analitica.si>
2022-12-16 10:06:20 +0100

    P2 way to slow..

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -24,9 +24,38 @@ for (int x = minX; x <= maxX; x++)
 
 int p1 = maxX - minX + 1 - lineDevices.Count;
 
+int minLimitX = 0;
+int minLimitY = 0;
+int maxLimitX = 4_000_000;
+int maxLimitY = 4_000_000;
+// int maxLimitX = 20;
+// int maxLimitY = 20;
+
+List<Device> orderedDevices = devices.OrderByDescending(x => x.Distance).ToList();
+List<Beacon> possibleBeacons = new();
+for (int y = minLimitY; y <= maxLimitY; y++)
+{
+  if (y % 10_000 == 0)
+  {
+    Console.WriteLine(y + "/" + maxLimitY);
+  }
+  if (possibleBeacons.Count > 0) break;
+  for (int x = minLimitX; x <= maxLimitX; x++)
+  {
+    if (possibleBeacons.Count > 0) break;
+    Beacon possibleBeacon = new(x, y);
+
+    if (!orderedDevices.Any(x => x.Sensor.CalculateManhattanDistanceTo(possibleBeacon) <= x.Distance))
+    {
+      possibleBeacons.Add(possibleBeacon);
+    }
+  }
+}
+
+long p2 = (long)possibleBeacons.First().X * 4_000_000 + possibleBeacons.First().Y;
 sw.Stop();
 
 
 Console.WriteLine($"Part one: {p1}");
-Console.WriteLine($"Part two: {0}");
+Console.WriteLine($"Part two: {p2}");
 Console.WriteLine($"Time: {sw.Elapsed}");
